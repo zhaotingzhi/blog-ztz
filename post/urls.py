751e6b955147b77334f5b38f24e9fd035c06db0c
zhaotingzhi <ztz100596@126.com>
2018-05-14 22:46:03 +0800

    分页显示，阅读全文，自定义过滤器markdown

--- post/urls.py
+++ post/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.index_view)
+    url(r'^$',views.index_view),
+    url(r'^page/(\d+)$',views.index_view),
+    url(r'^post/(\d+)$',views.post_view),
 
 
 ]
--- post/views.py
+++ post/views.py
@@ -1,8 +1,43 @@
-
-
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import *
+
 
 # Create your views here.
-def index_view(request):
+def get_post_by_num(num):
+    # 类型转换
+    num = int(num)
+    page_posts = Paginator(Post.objects.order_by('-created'), per_page=1)
+    # 返回当前页数据
+    # 首先判断当前页是否越界
+    if num <= 0:
+        num = 1
+    if num > page_posts.num_pages:
+        num = page_posts.num_pages
 
-    return render(request,'index.html')
+    # 生成页码数
+    # 起始数
+    start = int((num - 1) / 10) * 10 + 1
+    # 结尾数
+    end = start + 10
+    # 判断end是否越界
+    if end > page_posts.num_pages:
+        end = page_posts.num_pages + 1
+    return page_posts.page(num), range(start, end)
+
+
+def index_view(request, num='1'):
+    # 查询第一页的数据
+    page_posts, page_range = get_post_by_num(num)
+
+    return render(request, 'index.html', {'page_posts': page_posts, 'page_range': page_range})
+
+
+def get_post_by_id(postid):
+    return Post.objects.get(id=postid)
+
+
+def post_view(request, postid):
+    post = get_post_by_id(postid)
+
+    return render(request,'detail.html',{'post':post})
